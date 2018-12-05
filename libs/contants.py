d1ef2446c93765a08d51c594a46689ec8517e164
xia <xiayong411528@163.com>
2018-12-05 18:41:41 +0800

    第一次get接口整理完毕

--- libs/contants.py
+++ libs/contants.py
@@ -9,8 +9,10 @@ COMMON_PARA = {
 "UserId": 53
 }
 # 方法为get请求的接口
-# 1.2.配置参数接口接口：GET /app/init请求参数：无
+# 配置参数接口接口：GET /app/init请求参数：无
 URL_CONFIG = '/app/init'
+# 升级更新接口
+URL_CHECKUPDATE = '/app/checkUpdate'
 # 购买页
 URL_BY = '/buy'
 # 全国城市列表
@@ -20,5 +22,48 @@ URL_UPLOADSIGN = 'uploadsign'
 # 计费商品列表
 URL_GOODS = '/goods'
 # 活动列表
-URL_ACTIVITY = 'ac'
-
+URL_ACTIVITY = '/activity'
+# 资料页
+URL_ACTIVITY_DATA = '/activity/data'
+# 摄影师管理详情页
+URL_CAMERIST = '/camerist'
+# 直播原相册
+URL_PIC_ALBUM = '/picture/album'
+# 直播详情页
+URL_PIC_DETAIL = '/picture/detail'
+# 用户已上传图片列表
+URL_USER_PIC = '/user/picture'
+# 导播管理节目
+URL_EDITOR = '/editor/list'
+# 编辑室列表
+URL_EDIT = '/edit/list'
+# 预约摄影师列表
+URL_RESERVE = '/reserve/list'
+# 直播预sn
+URL_PREP = '/prep'
+# 分类列表
+URL_CATEGORY = '/category'
+# 我预约别人的
+URL_USER_MY = '/user/mybooking'
+# 别人预约我的
+URL_MYCUSTOMER = '/user/mycustomer'
+# 我的关注列表
+URL_USER_SUB = '/user/subscribe'
+# 二维码扫描
+URL_SCAN = '/scan'
+# 分享加强版
+URL_SHARE = '/share'
+# 设备码校验
+URL_DEVICE = '/device/verify'
+# 消息列表
+URL_MESSAGE = '/user/message'
+# 个人设置信息
+URL_USER_DEVICE = '/user/device'
+# banner
+URL_BANNER = '/banner'
+# 设备相册
+URL_DEVICE_ALBUM = '/device/album'
+# 验证邀请码
+URL_INVITE_CODE = '/invite-code'
+# 已结束的直播列表（在该直播下有身份）
+URL_ACTIVITY_END = '/activity/end/list'
